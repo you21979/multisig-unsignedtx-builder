1c956ece35b571160bae30e85068b8c06ff67c6e
you21979 <you2197901@gmail.com>
2017-07-28 18:07:49 +0900

    mod

--- bin/utxo_checker.js
+++ bin/utxo_checker.js
@@ -3,14 +3,21 @@ const fs = require("fs")
 const program = require('commander')
 const rp = require('request-promise')
 const task = require('promise-util-task')
+const sleep = require('@you21979/promise-sleep')
 
 program
   .version('0.0.1')
   .option('-f, --file <n>', 'utxo json file')
   .parse(process.argv);
 
+const req = (tx, vout, wait) => {
+    return rp("https://insight.bitpay.com/api/tx/"+tx).then(JSON.parse).then(res => {
+        return sleep(wait, [tx, vout, res.vout[vout].spentTxId === null])
+    })
+}
+
 const main = (opt) => {
-    return task.seq(opt.inputs.map(input => console.log(input)))
+    return task.seq(opt.inputs.map(input => () => req(input.tx_id, input.tx_vout, 1000))).filter(v => v[2] === false)
 }
 
 const getopt = (program) => {
@@ -21,4 +28,4 @@ const getopt = (program) => {
     }
 }
 
-main(getopt(program))
+main(getopt(program)).then(console.log)
