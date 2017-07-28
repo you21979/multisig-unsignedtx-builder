5958dce377ecb4f31fd0b7cc11a90760985531b3
you21979 <you2197901@gmail.com>
2017-07-28 16:18:12 +0900

    initial commit

---
+++ bin/rawtx_builder.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+const fs = require("fs")
+const program = require('commander')
+const main = require('../lib/main')
+const constant = require('../lib/constant')
+
+program
+  .version('0.0.1')
+  .option('-f, --file <n>', 'utxo json file')
+  .option('-a, --address <n>', 'spent address')
+  .option('-s, --spb <n>', 'transaction fees. satoshi per byte', (v) => parseInt(v), constant.SPB)
+  .option('-d, --divide <n>', 'transaction divide unit', (v) => parseInt(v), constant.DIVIDE_UNIT)
+  .parse(process.argv);
+
+const getopt = (program) => {
+    if(program.file === void 0) throw new Error("please input file")
+    if(program.address === void 0) throw new Error("please input address")
+    const config = JSON.parse(fs.readFileSync("./config/xpub.json", "utf8"))
+    const inputs = JSON.parse(fs.readFileSync(program.file, "utf8"))
+    const pubs = config.keys[0]
+    return {
+        pubs : pubs,
+        inputs : inputs,
+        spentaddress : program.address,
+        spb : program.spb,
+        divide_unit : program.divide,
+    }
+}
+
+main(getopt(program))
